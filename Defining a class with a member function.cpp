cde03ac27a8674c4afc4daca885a2f6f0cb98298
Shubham Mehta <ZeusEm@users.noreply.github.com>
2015-12-29 21:13:58 +0530

    Created the file
    Final, bug-free commit.

---
+++ Defining a class with a member function.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+class a
+{
+    public:
+    int show()
+    {
+        cout<<"Hello World!";
+    }
+};
+
+int main()
+{
+    system("cls");
+    a b;
+    b.show();
+    return 0;
+}
