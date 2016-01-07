931ba668c31a9363097e224589708f1233319e8b
Shubham Mehta <ZeusEm@users.noreply.github.com>
2016-01-07 20:03:25 +0530

    Created the file
    Final, bug-free commit.

---
+++ Initializing the elements of an array during its declaration.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int arr[]={10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+    for(int i=0; i<10; i++)
+        cout<<(i+1)<<" -> "<<arr[i]<<endl;
+    return 0;
+}
