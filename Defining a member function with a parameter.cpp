478df8577a30e8db19f54a91d577c790edc0eb9e
Shubham Mehta <ZeusEm@users.noreply.github.com>
2016-05-15 14:53:04 +0530

    Fixed a syntax error

--- Defining a member function with a parameter.cpp
+++ Defining a member function with a parameter.cpp
@@ -1,23 +1,25 @@
 #include <iostream>
 #include <string>
+#include <conio.h>
 
 using namespace std;
 
 class a
 {
-   public:
-    int show(string y)
-    {
-        cout<<endl<<endl<<"Welcome to the "<<y<<" course! We wish you speedy learning!"<<endl<<endl;
-    }
-} ;
+public:
+	void show(string y)
+	{
+		cout << endl << endl << "Welcome to the " << y << " course! We wish you speedy learning!" << endl << endl;
+	}
+};
 
 int main()
 {
-    a b;
-    cout<<"Enter your course: ";
-    string z;
-    getline(cin, z);
-    b.show(z);
-    return 0;
+	a b;
+	cout << "Enter your course: ";
+	string z;
+	getline(cin, z);
+	b.show(z);
+	getch();
+	return 0;
 }
